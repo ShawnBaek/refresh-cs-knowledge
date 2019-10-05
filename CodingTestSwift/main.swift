6027daea5017c8d909f3a02fb0c4c0eafc0c0076
ShawnBaek <shawn@shawnbaek.com>
2019-10-05 10:21:22 +0900

    Initial Commit

---
+++ CodingTestSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodingTestSwift
+//
+//  Created by BaekSungwook on 10/5/19.
+//  Copyright © 2019 BaekSungwook. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
